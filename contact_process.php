d59d092169915bd8799332392513662b2df0abb3
Rameen Sahoo <rameensahoo@Rameens-iMac.local>
2023-01-29 12:56:27 -0800

    fix footer padding

--- contact_process.php
+++ contact_process.php
@@ -1,11 +1,11 @@
 <?php
 
-    $to = "handtfloors@gmail.com";
+    $to = "rameensahoo@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
     $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
+    $message = $_REQUEST['message'];
 
     $headers = "From: $from";
 	$headers = "From: " . $from . "\r\n";
@@ -13,7 +13,7 @@
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from your Bitmap Photography.";
+    $subject = "You have a message for H & T Flooring.";
 
     $logo = 'img/logo.png';
     $link = '#';
@@ -26,9 +26,9 @@
 	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
 	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
 	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
+	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>";
 	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
+	$body .= "<tr><td colspan='2' style='border:none;'>{$message}</td></tr>";
 	$body .= "</tbody></table>";
 	$body .= "</body></html>";
 
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+if(isset($_POST['submit'])) {
+    echo 'Form submitted'
+}
+?>
