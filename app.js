8d348551e3eedb3d12f47c6aed4afa7b4e7b4a55
Rameen Sahoo <rameensahoo@Rameens-iMac.local>
2023-01-29 19:40:45 -0800

    initialize repo

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+app.listen(3000, function () {
+    console.log("Server is running on localhost3000");
+});
