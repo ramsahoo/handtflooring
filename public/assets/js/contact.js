22f71a7e49074ec404efa1fcfa669210c14ca45f
Rameen Sahoo <rameensahoo@Rameens-iMac.local>
2023-01-29 18:53:56 -0800

    edited the contact form

--- public/assets/js/contact.js
+++ public/assets/js/contact.js
@@ -1,85 +1,42 @@
 $(document).ready(function(){
     
-    (function($) {
-        "use strict";
-
-    
-    jQuery.validator.addMethod('answercheck', function (value, element) {
-        return this.optional(element) || /^\bcat\b$/.test(value)
-    }, "Correct answer is required.");
-
-    // validate contactForm form
-    $(function() {
-        $('#contactForm').validate({
-            rules: {
-                name: {
-                    required: true,
-                    minlength: 2
-                },
-                subject: {
-                    required: true,
-                    minlength: 4
-                },
-                number: {
-                    required: true,
-                    minlength: 5
-                },
-                email: {
-                    required: true,
-                    email: true
-                },
-                message: {
-                    required: true,
-                    minlength: 20
-                }
-            },
-            messages: {
-                name: {
-                    required: "Name is required.",
-                    minlength: "Name must consist of at least 2 characters."
-                },
-                subject: {
-                    required: "Subject is required.",
-                    minlength: "Subject must consist of at least 4 characters."
-                },
-                number: {
-                    required: "Number is required",
-                    minlength: "Number must consist of at least 5 characters."
-                },
-                email: {
-                    required: "Email is required."
-                },
-                message: {
-                    required: "Message cannot be blank.",
-                    minlength: "More content is required."
-                }
-            },
-            submitHandler: function(form) {
-                $(form).ajaxSubmit({
-                    type:"POST",
-                    data: $(form).serialize(),
-                    url:"contact_process.php",
-                    success: function() {
-                        $('#contactForm :input').attr('disabled', 'disabled');
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $(this).find(':input').attr('disabled', 'disabled');
-                            $(this).find('label').css('cursor','default');
-                            $('#success').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#success').modal('show');
-                        })
-                    },
-                    error: function() {
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $('#error').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#error').modal('show');
-                        })
-                    }
-                })
-            }
-        })
-    })
+    $("#send").on('click', function(e){
         
- })(jQuery)
-})
+        $("#send").text('Please Wait..');
+        $("#send").attr('disabled', true);
+        
+        $.ajax({
+                url: "#contact_process.php",
+            method:"POST",
+                dataType:"JSON",
+                data:{
+                    name:$("#name").val(),
+                    email:$("#email").val(),
+                    phone:$("#phone").val(),
+                    subject:$("#subject").val(),
+                    message:$("#message").val()
+                },
+                success:function(res){
+                    $("#send").text('Send');
+                    $("#send").attr('disabled', false);
+                    if (res == 1){
+                        $("#status").html('Message Sent Successfully.');
+                    }else if(res == 0){
+                        $("#status").html('Message Failed.');
+                    }else if(res == 2){
+                        $("#status").html('Empty Fields Detected.');
+                    }else if(res == 3){
+                        $("#status").html('Invalid Email Address.');
+                    }
+                    
+                },
+                error: function(err){
+                    
+                    $('#status').html( "Network Connection Error.");
+                }
+                });
+    
+        
+    });
+    
+    });
