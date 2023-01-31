2b890a049a9918d9cca3407eaa02bbe25a6e6831
Rameen Sahoo <rameensahoo@Rameens-iMac.local>
2023-01-30 23:37:07 -0800

    fixed footer padding on viewport

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/public/index.html");
 });
 app.listen(3000, function () {
     console.log("Server is running on localhost3000");
