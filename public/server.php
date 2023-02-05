891f5a85228e07da84a20960a8598bf13304f32c
Rameen Sahoo <rameensahoo@Rameens-iMac.local>
2023-02-04 23:32:36 -0800

    working contact form

--- public/server.php
+++ public/server.php
@@ -8,48 +8,76 @@ use \PHPMailer\PHPMailer\Exception;
 
 $mail=new PHPMailer(true); // Passing `true` enables exceptions
 
-try {
-    $from = "";
-	$to = "rameensahoo@gmail.com";
-	$name = $_REQUEST['name'];
-    $email = $_REQUEST['email'];
-	$number = $_REQUEST['number'];
-    $subject = $_REQUEST['subject'];
-    $message = $_REQUEST['message'];
 
-    $headers = "From: $name";
+try {
+    //settings
+    $mail->SMTPDebug=2; // Enable verbose debug output
+    $mail->isSMTP(); // Set mailer to use SMTP
+    $mail->Host='smtp.gmail.com';
+    $mail->SMTPAuth=true; // Enable SMTP authentication
+    $mail->Username='rameensahoo@gmail.com'; // SMTP username
+    $mail->Password='agprxqfxyhlitihr'; // SMTP password
+    $mail->SMTPSecure='ssl';
+    $mail->Port=465;
+
+	//variables
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$number = $_POST['number'];
+	$subject = $_POST['subject'];
+	$message = $_POST['message'];
+
+	//from email address and name
+    $mail->FromName = $name = $_POST['name'];;
+	$mail->From = ('client@handtfloors.com');
+
+	//to or recipient address and name
+	$mail->addAddress("handtfloors@gmail.com", "Mihai Mironov");
+
+	//Address to which recipient will reply
+	$mail->addReplyTo($email = $_REQUEST['email']);	
+
+	//CC 
+	$mail->addCC("rameensahoo@gmail.com");
+
+    //Subject
+	$mail->Subject = $subject = $_REQUEST['subject'];
+
+	$headers = "From: $name";
 	$headers = "From: " . $email . "\r\n";
 	$headers .= "Reply-To: ". $email . "\r\n";
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message for H & T Flooring.";
-
-    $logo = 'img/logo.png';
-    $link = 'https://handtfloors.com';
-
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+	// HTML Body
+	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>H & T Flooring Contact Request</title></head><body>";
 	$body .= "<table style='width: 100%;'>";
 	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
 	$body .= "</td></tr></thead><tbody><tr>";
 	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
 	$body .= "<td style='border:none;'><strong>Email:</strong> {$email}</td>";
 	$body .= "<td style='border:none;'><strong>Phone Number:</strong> {$number}</td>";
+	$body .= "</div>";
 	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>";
+	$body .= "<tr><td colspan='2' style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>";
 	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$message}</td></tr>";
+	$body .= "<tr><td colspan='2' style='border:none;'><strong>Message:</strong> {$message}</td></tr>";
 	$body .= "</tbody></table>";
 	$body .= "</body></html>";
+    $mail->Body = $body;
+	$mail->AltBody = $body;
+	$mail->isHTML(true); // Set email format to HTML
+	echo $body;
 
-    $send = mail($from, $to, $headers, $subject, $body);
+	//send mail
+    $mail->send();
 
-	echo 'Message has been sent. Thank you!';
+	//prompts
+    echo 'Message has been sent';
+} 
+catch(Exception $e) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: '.$mail->ErrorInfo;
 }
-	catch(Exception $e) {
-		echo 'Message could not be sent.';
-		echo 'Mailer Error: '.$mail->ErrorInfo;
-	}
 
 ?>
